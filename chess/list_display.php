fd644474899d5feac569a71cd01ca4a53306f92a
deific-jam <69472336+deific-jam@users.noreply.github.com>
2021-03-15 16:14:15 +0530

    Update list_display.php

--- chess/list_display.php
+++ chess/list_display.php
@@ -1,24 +1,24 @@
 <?php
-	$servername = "localhost";
-		$dbusername = "id16371456_userwebsite";
-		$dbpassword = "/_K@S\3q7>?Im0kW";
-		$dbname = "id16371456_dbparticipants";
+	$server = "localhost";
+    $dbuname = "id16371456_dbadmin";
+    $dbpass = "Y)e7)ZGKtJ8LTu?k";
+    $dbname = "id16371456_dbparticipants";
 	
 	// Create connection
-		$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+		$conn = new mysqli($server, $dbuname, $dbpass, $dbname);
 		// Check connection
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}else{
 			$fetchdata = "SELECT id, name, username From part-list";
-			$data = $conn-query($fetchdata);
+			$data = $conn->query($fetchdata);
 			
 			if($data->num_rows > 0){
 				while($rows = $data->fetch_assoc)()){
 					echo "<br> ID: ".$row[""]."<br> Name: ".$rows["name"]."<br> Username: ".$row["username"]."<br>";
 				}
 			}else{
-				echo "No one has registered yet."
+				echo "No one has registered yet.";
 			}
 		}
 	$conn->close();
